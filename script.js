1feda3ae61674d3539b03601c4f2b7b72545d65d
ry brown <ryan.alex.brown@gmail.com>
2020-08-16 12:32:57 -0700

    trying to find a way to link my characters

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // Assignment code here
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var number = ["1","2","3","4","5","6","7","8","9","0"];
+var symbol = ["!","@","#","$","%","^","&","*","(",")","-","+"];
+
+
+
 function generatePassword (){
 var passwordLengthConfirm = window.prompt("How many characters would you like your password to be? It must be between 8 and 128.");
 if (passwordLengthConfirm < 8 || passwordLengthConfirm > 128){
@@ -18,6 +25,7 @@ if (!upperCaseConfirm && !lowerCaseConfirm && !numberConfirm && !symbolConfirm){
   symbolConfirm = window.confirm ("Would you like to use symbols?");
 
 }
+
 
 var passwordChar = []
 if (upperCaseConfirm){
