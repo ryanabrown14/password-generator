a5f6affdb34b923abc134d43d59cb4a63b56e88f
ry brown <ryan.alex.brown@gmail.com>
2020-08-15 22:38:22 -0700

    very basic variables up

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 // Assignment code here
+var passwordLength = window.prompt("How many characters would you like your password to be?");
+var upperCase = window.confirm ("Would you like to use upercase?");
+var lowerCase = window.confirm ("Would you liek to use lowercase?");
+var number = window.confirm ("Would you like to use numbers?");
+var symbol = window.confirm ("Would you like to use symbols?");
+
 
 
 // Get references to the #generate element
